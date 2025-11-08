132d657f4148e6001760dc4348e169204e9b6a8d
Gabriel Vesperman <90653423+Gabe2008@users.noreply.github.com>
2025-11-08 14:53:50 -0600

    Made it so the start launch should run for enough time to build up to top speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms.java
@@ -8,20 +8,33 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Mechanisms {
-    //class to create a wrist
     public static class Launcher {
+        private long startingTime = System.currentTimeMillis();
+        private long currentTime = 0;
+        private final int rampUpTime =50000;
+
         public DcMotor launcher1;
         public DcMotor launcher2;
         public Launcher(HardwareMap hardwareMap) {
             launcher1 = hardwareMap.get(DcMotor.class, "launcher1");
             launcher2 = hardwareMap.get(DcMotor.class, "launcher2");
         }
+
         public class StartLaunch implements Action {
+            private boolean initialized = false;
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
-                launcher1.setPower(1);
-                launcher2.setPower(-1);
-                return launcher1.getPower()<1;
+                if (!initialized) {
+                    if(launcher1.getPower()==1){
+                        return false;
+                    }
+                    launcher1.setPower(1);
+                    launcher2.setPower(-1);
+                    startingTime = System.currentTimeMillis();
+                    initialized = true;
+                }
+                currentTime = System.currentTimeMillis() - startingTime;
+                return currentTime < rampUpTime;
             }
         }
         public Action startLaunch() {
@@ -32,7 +45,7 @@ public class Mechanisms {
             public boolean run(@NonNull TelemetryPacket packet) {
                 launcher1.setPower(0);
                 launcher2.setPower(0);
-                return launcher1.getPower()>0;
+                return false;
             }
         }
         public Action stopLauncher() {
