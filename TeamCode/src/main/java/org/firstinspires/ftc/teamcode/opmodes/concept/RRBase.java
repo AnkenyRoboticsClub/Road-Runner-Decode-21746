2dadfd6c8852ef3c6d32cfba2188acfcd56fab8f
Deadlypants009 <deadlypants009@gmail.com>
2025-11-11 10:44:31 -0600

    RRBase Custom Actions
    Added and explained custom actions example in the RRBase script.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/concept/RRBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/concept/RRBase.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmodes.concept;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.InstantFunction;
+import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
@@ -12,14 +16,19 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Disabled
 @Autonomous(name = "RR_BASE", group = "Autonomous")
 public class RRBase extends LinearOpMode {
 
+    DcMotor lift = hardwareMap.get(DcMotor.class, "liftMotor");
+
+    Servo claw = hardwareMap.get(Servo.class, "clawServo");
+
     // instant function example
-    Servo claw;
     public class SetClawPos implements InstantFunction {
 
         double targetPosition;
@@ -34,6 +43,43 @@ public class RRBase extends LinearOpMode {
         public void run() {
                 claw.setPosition(this.targetPosition);
         }
+    }
+
+    // custom action example
+    public class LiftTo implements Action {
+        private double targetPos;
+        private double direction;
+        private double speed = 0.5;
+        private double tolerance = 10;
+        private boolean initialized = false;
+        public LiftTo(double position) {
+            this.targetPos = position;
+
+            if (lift.getCurrentPosition() < this.targetPos) {
+                this.direction = 1;
+            } else {
+                this.direction = -1;
+            }
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            // on first time through loop set the power to the lift
+            if (!this.initialized) {
+                lift.setPower(this.speed * this.direction);
+                this.initialized = true;
+            }
+
+            // Continue running if outside tolerance range
+            if (Math.abs(lift.getCurrentPosition() - targetPos) > tolerance) {
+                return true;
+            }
+
+            // once lift reaches the target position stop power and exit loop
+            lift.setPower(0);
+            return false;
+        }
+
     }
 
     @Override
@@ -58,12 +104,21 @@ public class RRBase extends LinearOpMode {
                 .strafeTo(new Vector2d(30, 30))
 
                 // you can also add your own custom functions using an instant function
+                // instant functions are similar to custom actions except for they can't
+                // run in parallel with another action
                 .stopAndAdd(new SetClawPos(0.5))
                 .build();
 
         // sequential action runs your path one at a time
         // use parallel action to do multiple paths at once
         Actions.runBlocking(new SequentialAction(path));
+
+        // parallel actions run multiple actions at once
+        // will set the lift position to 100 while running the path
+        Actions.runBlocking(new ParallelAction(
+                path,
+                new LiftTo(100)
+        ));
     }
 
 }
