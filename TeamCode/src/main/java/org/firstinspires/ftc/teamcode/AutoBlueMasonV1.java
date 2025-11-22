62c94eb08e83dac3dadae28b162534454f01fa94
Robotics12345678 <3033418@students.ankenyschools.org>
2025-11-22 11:01:29 -0600

    Adding launcher to AutoBlueMason

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueMasonV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueMasonV1.java
@@ -18,11 +18,39 @@ public class AutoBlueMasonV1 extends LinearOpMode {
         Pose2d initialPose = new Pose2d(-48, -48, Math.toRadians(52));
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+        Mechanisms.Launcher launcher = new Mechanisms.Launcher(hardwareMap);
+        Mechanisms.Gate gate = new Mechanisms.Gate(hardwareMap);
 
         waitForStart();
 
         if (isStopRequested()) return;
 
+        initialFieldPositioning(drive, initialPose);
+        launchArtifact(launcher, gate);
+        finalFieldPosition();
+
+    }
+
+    private void launchArtifact(Mechanisms.Launcher launcher,Mechanisms.Gate gate)
+    {
+        Actions.runBlocking(launcher.setLauncherPower(0.55));
+        //Wait for launcher to speed up to the desired speed
+        sleep(500);
+        //Release ball - turn on the motor that opens the gate
+        Actions.runBlocking(gate.openGate());
+        //Stop launcher
+        Actions.runBlocking(launcher.stopLauncher());
+        //Close - turn on the motor that opens the gate opposite speed
+        Actions.runBlocking(gate.closeGate());
+
+    }
+
+    private void finalFieldPosition()
+    {}
+
+
+    private void initialFieldPositioning(MecanumDrive drive, Pose2d initialPose)
+    {
         Action path = drive.actionBuilder(initialPose)
                 .lineToX(-30)
                 .turn(Math.toRadians(180))
@@ -30,8 +58,9 @@ public class AutoBlueMasonV1 extends LinearOpMode {
                 .strafeTo(new Vector2d(0, -20))
                 .build();
 
-        Actions.runBlocking(new SequentialAction(path));
 
+        Actions.runBlocking(path);
     }
+
 
 }
